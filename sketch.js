10e4f96426d3ceb2643086d9fc7f6e97919c9b89
yusuf17-10 <64640684+yusuf17-10@users.noreply.github.com>
2020-06-13 19:07:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+var ball,datebase,position;
+
+function setup(){
+    database=firebase.database();
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+
+    var ballPosition=database.ref('ball/position');
+
+    ballPosition.on("value",readPos,showError);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+    database.ref('ball/position').set({
+        x : ball.x + x,
+        y : ball.y + y
+    })
+    
+}
+
+function showError(){
+    console.log("Error");
+}
+
+function readPos(data){
+    position=data.val();
+    ball.x=position.x;
+    ball.y=position.y;
+
+}
